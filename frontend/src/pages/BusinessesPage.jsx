edf28c945182e62a24e1de374496fd5a12ab9530
Leonardo2020-col <leonardopareja80@gmail.com>
2025-06-11 16:46:49 -0500

    actualizando ruta

--- frontend/src/pages/BusinessesPage.jsx
+++ frontend/src/pages/BusinessesPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import BusinessTable from '../components/BusinessTable/BusinessTable';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/Navbar/Navbar';
 
 const BusinessesPage = () => {
   return (
